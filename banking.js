f45b3c3a3328d0ec2b6775c6fa687732d8187843
Mosiur12 <mrsojib9600@gmail.com>
2022-02-11 20:45:24 +0600

    another changes

---
+++ banking.js
@@ -0,0 +1,5 @@
+document.getElementById('Deposit-button').addEventListener('click',function(){
+    const depositField = document.getElementById('depositMoney');
+    const amountDeposit = depositField.value;
+    
+})
